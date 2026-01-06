96e306ba58e18caf4620828ff9d3d784ae7f92cb
Nikaidou Haruki <nharuki@pm.me>
2026-01-06 23:02:25 +0900

    implement queries in user account table

--- modules/auth/src/entities/db/user_account.rs
+++ modules/auth/src/entities/db/user_account.rs
@@ -1,3 +1,6 @@
+use framework::sqlx::DatabaseProcessor;
+use kanau::processor::Processor;
+use tracing::instrument;
 use uuid::Uuid;
 
 #[derive(Debug, Clone, Eq, PartialEq, sqlx::FromRow)]
@@ -7,4 +10,81 @@ pub struct UserAccount {
     pub email: String,
     pub created_at: time::PrimitiveDateTime,
     pub updated_at: time::PrimitiveDateTime,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct FindUserAccountById {
+    pub id: Uuid,
+}
+
+impl Processor<FindUserAccountById, Result<Option<UserAccount>, sqlx::Error>>
+    for DatabaseProcessor
+{
+    #[instrument(skip_all, name = "SQL:FindUserAccountById", err)]
+    async fn process(
+        &self,
+        input: FindUserAccountById,
+    ) -> Result<Option<UserAccount>, sqlx::Error> {
+        sqlx::query_as!(
+            UserAccount,
+            r#"
+            SELECT id, name, email, created_at, updated_at
+            FROM "auth"."user_account"
+            WHERE id = $1
+            "#,
+            input.id
+        )
+        .fetch_optional(self.db())
+        .await
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct UpdateUserEmail {
+    pub id: Uuid,
+    pub email: String,
+}
+
+impl Processor<UpdateUserEmail, Result<UserAccount, sqlx::Error>> for DatabaseProcessor {
+    #[instrument(skip_all, name = "SQL:UpdateUserEmail", err)]
+    async fn process(&self, input: UpdateUserEmail) -> Result<UserAccount, sqlx::Error> {
+        sqlx::query_as!(
+            UserAccount,
+            r#"
+            UPDATE "auth"."user_account"
+            SET email = $2, updated_at = NOW()
+            WHERE id = $1
+            RETURNING id, name, email, created_at, updated_at
+            "#,
+            input.id,
+            &input.email
+        )
+        .fetch_one(self.db())
+        .await
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct UpdateUserName {
+    pub id: Uuid,
+    pub name: Option<String>,
+}
+
+impl Processor<UpdateUserName, Result<UserAccount, sqlx::Error>> for DatabaseProcessor {
+    #[instrument(skip_all, name = "SQL:UpdateUserName", err)]
+    async fn process(&self, input: UpdateUserName) -> Result<UserAccount, sqlx::Error> {
+        sqlx::query_as!(
+            UserAccount,
+            r#"
+            UPDATE "auth"."user_account"
+            SET name = $2, updated_at = NOW()
+            WHERE id = $1
+            RETURNING id, name, email, created_at, updated_at
+            "#,
+            input.id,
+            input.name
+        )
+        .fetch_one(self.db())
+        .await
+    }
 }
