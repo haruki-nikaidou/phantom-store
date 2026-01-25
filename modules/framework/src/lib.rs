8452e529223e73aae5dd0263dc70f34b31dd456d
Nikaidou Haruki <nharuki@pm.me>
2026-01-25 19:04:54 +0900

    add `now_time` function to framework

--- modules/framework/src/lib.rs
+++ modules/framework/src/lib.rs
@@ -8,3 +8,8 @@ pub mod redis;
 pub mod sqlx;
 
 pub use error::Error;
+
+pub fn now_time() -> time::PrimitiveDateTime {
+    let now = time::OffsetDateTime::now_utc();
+    time::PrimitiveDateTime::new(now.date(), now.time())
+}
