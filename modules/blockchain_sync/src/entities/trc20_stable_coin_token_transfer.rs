2eec117897c91afa9ca293f727af27e55a68f943
Nikaidou Haruki <nharuki@pm.me>
2026-01-26 20:21:14 +0900

    refactor trc20 module: change mod visibility to public and add FromRow derive for structs

--- modules/blockchain_sync/src/entities/trc20_stable_coin_token_transfer.rs
+++ modules/blockchain_sync/src/entities/trc20_stable_coin_token_transfer.rs
@@ -1,5 +1,6 @@
 use crate::utils::supported_tokens::StableCoinName;
 
+#[derive(Debug, Clone, PartialEq, Eq, sqlx::FromRow)]
 pub struct Trc20StableCoinTokenTransfer {
     pub id: i64,
     pub token_name: StableCoinName,
